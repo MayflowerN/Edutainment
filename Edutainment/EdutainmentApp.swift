2a5fd5454eb0598ee047d7e9e6b09b968dabf953
EllieNguyen <ellieN1235@gmail.com>
2024-09-10 01:36:03 -0700

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Ellie on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
